141a90a8e2aed82892eebff8344ab986b2aa049f
matt-visentin <134010450+matt-visentin@users.noreply.github.com>
2023-06-03 11:23:06 +0200

    Created list of pokemons

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+let pokemonList = [ //pokemons database for pokedex
+    {
+        name: 'Bulbasaur',
+        id: 1,
+        type: ['grass', 'poison'],
+        height: 70,
+    },
+    {
+        name: 'Charmander',
+        id: 4,
+        type: ['fier'],
+        height: 60,
+    },
+    {
+        name: 'Squirtle',
+        id: 7,
+        type: ['water'],
+        height: 50,
+    },
+    {
+        name: 'Caterpie',
+        id: 10,
+        type: ['bug'],
+        height: 30,
+    },
+    {
+        name: 'Weedle',
+        id: 13,
+        type: ['bug', 'poison'],
+        height: 30,
+    },
+    {
+        name: 'Pidgey',
+        id: 16,
+        type: ['flying', 'normal'],
+        height: 30,
+    },
+    {
+        name: 'Rattata',
+        id: 19,
+        type: ['normal'],
+        height: 30,
+    },
+    {
+        name: 'Spearow',
+        id: 21,
+        type: ['flying', 'normal'],
+        height: 30,
+    },
+    {
+        name: 'Ekans',
+        id: 23,
+        type: ['poison'],
+        height: 200,
+    },
+    {
+        name: 'Pikachu',
+        id: 25,
+        type: ['electric'],
+        height: 40,
+    },
+    {
+        name: 'Sandshrew',
+        id: 27,
+        type: ['ground'],
+        height: 60,
+    },
+    {
+        name: 'Nidoran',
+        id: 29,
+        type: ['poison'],
+        height: 40,
+    },
+];
